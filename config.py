0bf3a4b9fb980729ecf2f012c792c33b5552a78c
aritra0786 <160320152+aritra0786@users.noreply.github.com>
2024-11-09 10:47:05 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SHARP
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7791716240:AAG-6qK5TwXnVGY2EAJY0L5WnJem1HFRZQ8")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("VOIDxARITRAXD")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7323731793")
+
+
+
+
+
+
+
+  
